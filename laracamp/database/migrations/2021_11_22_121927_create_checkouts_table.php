0be772bf20959a1e6c29e68583fa3369d9e51c44
mindatama <mindatama@gmail.com>
2021-11-22 20:30:36 +0800

    migrate & model: Campbenefit & Checkout

--- laracamp/database/migrations/2021_11_22_121927_create_checkouts_table.php
+++ laracamp/database/migrations/2021_11_22_121927_create_checkouts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCampbenefitsTable extends Migration
+class CreateCheckoutsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,16 @@ class CreateCampbenefitsTable extends Migration
      */
     public function up()
     {
-        Schema::create('campbenefits', function (Blueprint $table) {
+        Schema::create('checkouts', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('camp_id')->unsigned();
-            $table->string('name');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('camp_id')->constrained();
+            $table->string('card_number',20);
+            $table->date('expired');
+            $table->string('cvc',3);
+            $table->boolean('is_paid')->default(false);
             $table->timestamps();
             $table->softDeletes();
-
-            $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
         });
     }
 
@@ -31,6 +33,6 @@ class CreateCampbenefitsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('campbenefits');
+        Schema::dropIfExists('checkouts');
     }
 }
