0184726b4f6b7968addc0fc4e8875e8afb31ab31
mindatama <mindatama@gmail.com>
2022-01-06 17:31:10 +0800

    admin integrasi dashboard

--- laracamp02/app/Http/Controllers/Admin/DashboardController.php
+++ laracamp02/app/Http/Controllers/Admin/DashboardController.php
@@ -11,7 +11,7 @@ class DashboardController extends Controller
 {
     public function index(){
         $checkouts = Checkout::with('Camp')->get();
-        return view('user.dashboard', [
+        return view('admin.dashboard', [
             'checkouts' => $checkouts
         ]);
     }
