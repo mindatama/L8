2a3f3802adf5a029ed671e869d0d4b66dbcaf004
Wijatmoko Mindatama <wijatmoko.m@bukitmakmur.com>
2022-01-27 17:53:10 +0800

    tambah comment biar update github aja

--- laracamp02/resources/views/auth/login.blade.php
+++ laracamp02/resources/views/auth/login.blade.php
@@ -43,7 +43,7 @@
             <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Lupa sandi?') }}
+                        {{ __('Anda Lupa sandi?') }}
                     </a>
                 @endif
 
