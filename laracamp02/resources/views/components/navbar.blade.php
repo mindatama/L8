704d320c3e1578ec9d53113082201511101cb97f
mindatama <mindatama@gmail.com>
2022-01-30 17:39:53 +0800

    tambah navbar assign job

--- laracamp02/resources/views/components/navbar.blade.php
+++ laracamp02/resources/views/components/navbar.blade.php
@@ -9,6 +9,9 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
+                    <a class="nav-link" href="{{route('welcome')}}">Assign Job</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" aria-current="page" href="{{route('dashboard')}}">Dashboard</a>
                 </li>
                 <li class="nav-item">
