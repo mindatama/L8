a2a0d74b99eb55950336abd8d6bef81eaeadf331
mindatama <mindatama@gmail.com>
2022-02-09 17:48:21 +0800

    ganti body email lagi, lebih detail

--- laracamp02/resources/views/emails/checkout/afterCheckout.blade.php
+++ laracamp02/resources/views/emails/checkout/afterCheckout.blade.php
@@ -1,9 +1,16 @@
 @component('mail::message')
-# Register Jobpending: {{$checkout->Camp->title}}  {{$checkout->judul}}
+# Register Jobpending: {{$checkout->Camp->title}} judul {{$checkout->judul}}
 
 Hey {{$checkout->User->name}}
 <br>
-thanks for upload job {{$checkout->Camp->title}}, saya dan anda selamat setiap hari
+thanks for upload job {{$checkout->Camp->title}}, 
+berikut detail pekerjaan : 
+pembuat : {{$checkout->User->name}}
+judul : {{$checkout->judul}}
+deskripsi : {{$checkout->deskripsi}}
+tanggal pekerjaan : {{$checkout->expired}}
+estimasi pekerjaan (hari) : {{$checkout->cvc}}
+saya dan anda selamat setiap hari
 
 @component('mail::button', ['url' => route('dashboard', $checkout->id)])
 Going to Dashboard
