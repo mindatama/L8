25454aa5e1fbcd1b03af52b7617e231d5b7de707
mindatama <mindatama@gmail.com>
2022-02-09 20:21:18 +0800

    tambah deskripsi judul

--- laracamp02/app/Mail/Checkout/AfterCheckout.php
+++ laracamp02/app/Mail/Checkout/AfterCheckout.php
@@ -28,7 +28,7 @@ class AfterCheckout extends Mailable
      */
     public function build()
     {
-        return $this->subject("Jobpending: {$this->checkout->Camp->title} berhasil dibuat")->markdown('emails.checkout.afterCheckout', [
+        return $this->subject("Jobpending: {$this->checkout->Camp->title} {$this->checkout->judul} berhasil dibuat")->markdown('emails.checkout.afterCheckout', [
             'checkout' =>$this->checkout
         ]);
     }
