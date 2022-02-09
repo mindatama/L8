e6641332aabdeaebafb2303f0bcadbad6dcc7a80
mindatama <mindatama@gmail.com>
2022-02-09 17:56:00 +0800

    tambah br di body email, isinya lebih baik

--- laracamp02/resources/views/emails/checkout/afterCheckout.blade.php
+++ laracamp02/resources/views/emails/checkout/afterCheckout.blade.php
@@ -3,13 +3,13 @@
 
 Hey {{$checkout->User->name}}
 <br>
-thanks for upload job {{$checkout->Camp->title}}, <br>
+terimakasih telah upload pekerjaan {{$checkout->Camp->title}}, <br>
 berikut detail pekerjaan : <br>
 pembuat : {{$checkout->User->name}} <br>
 judul : {{$checkout->judul}} <br>
 deskripsi : {{$checkout->deskripsi}} <br>
-tanggal pekerjaan : {{$checkout->expired}} <br>
-estimasi pekerjaan (hari) : {{$checkout->cvc}} <br>
+tanggal mulai : {{$checkout->expired}} <br>
+estimasi pekerjaan : {{$checkout->cvc}} hari<br>
 saya dan anda selamat setiap hari
 
 @component('mail::button', ['url' => route('dashboard', $checkout->id)])
