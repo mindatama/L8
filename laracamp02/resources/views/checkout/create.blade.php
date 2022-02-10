1daaaec6cab2ebcd75855d05b554e9da0fa49bdc
mindatama <mindatama@gmail.com>
2022-02-10 13:44:47 +0800

    ganti deskripsi dari input ke textarea

--- laracamp02/resources/views/checkout/create.blade.php
+++ laracamp02/resources/views/checkout/create.blade.php
@@ -54,7 +54,7 @@
                             </div>
                             <div class="mb-4">
                                 <label class="form-label">Deskripsi pekerjaan</label>
-                                <textarea rows="3" name="deskripsi" type="text" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{old('deskripsi') ?: Auth::user()->deskripsi}}" />
+                                <textarea rows="3" name="deskripsi" type="text" class="form-control {{$errors->has('name') ? 'is-invalid' : ''}}" value="{{old('deskripsi') ?: Auth::user()->deskripsi}}"></textarea>
                                 @if ($errors->has('deskripsi'))
                                     <p class="text-danger">{{$errors->first('deskripsi')}}</p>    
                                 @endif
